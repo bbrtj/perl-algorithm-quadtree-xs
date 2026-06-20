27e85eec15c11ef7febe838918269774e04b42c1
bbrtj <bbrtj.pro@gmail.com>
2026-06-20 13:58:43 +0200

    Add a new implementation which avoids using backreferences

---
+++ qtbase.h
@@ -0,0 +1,70 @@
+#ifndef QTBASE_H
+#define QTBASE_H
+
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+typedef struct QuadTreeNode QuadTreeNode;
+typedef struct QuadTreeRootNode QuadTreeRootNode;
+typedef struct DynArr DynArr;
+typedef enum ShapeType ShapeType;
+typedef struct Shape Shape;
+
+struct QuadTreeNode {
+	QuadTreeNode *children;
+	QuadTreeNode *parent;
+	DynArr *values;
+	double xmin, ymin, xmax, ymax;
+	bool has_objects;
+};
+
+struct QuadTreeRootNode {
+	QuadTreeNode *node;
+	HV *backref;
+};
+
+struct DynArr {
+	void **ptr;
+	unsigned int count;
+	unsigned int max_size;
+};
+
+enum ShapeType {
+	shape_rectangle,
+	shape_circle
+};
+
+struct Shape {
+	ShapeType type;
+	double dimensions[4];
+};
+
+
+typedef enum ShapeType ShapeType;
+
+DynArr* create_array();
+void destroy_array(DynArr* arr);
+void destroy_array_SV(DynArr* arr);
+void clear_array_SV(DynArr *arr);
+void push_array(DynArr *arr, void *ptr);
+void push_array_SV(DynArr *arr, SV *ptr);
+QuadTreeNode* create_nodes(int count, QuadTreeNode *parent);
+void destroy_node(QuadTreeNode *node);
+void clear_has_objects (QuadTreeNode *node);
+QuadTreeRootNode* create_root();
+QuadTreeRootNode* create_root_nobackref();
+void store_backref(QuadTreeRootNode *root, QuadTreeNode* node, SV *value);
+void node_add_level(QuadTreeNode* node, double xmin, double ymin, double xmax, double ymax, int depth);
+void find_nodes(QuadTreeNode *node, AV *ret, Shape *param);
+void fill_nodes(QuadTreeRootNode *root, QuadTreeNode *node, SV *value, Shape *param);
+void fill_nodes_nobackref(QuadTreeNode *node, SV *value, Shape *param);
+void clear_tree(QuadTreeRootNode *root);
+
+/* XS helpers */
+
+SV* get_hash_key (HV* hash, const char* key);
+QuadTreeRootNode* get_root_from_perl(SV *self);
+
+#endif
+
